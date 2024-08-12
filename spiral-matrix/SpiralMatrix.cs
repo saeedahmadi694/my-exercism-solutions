6aca8eadb2c62b907e15d0b9225a2e4792efcd6f
Saeed Ahmadi <saeedahmadi694@gmail.com>
2024-08-12 21:58:04 +0330

    init spiral-matrix

---
+++ spiral-matrix/SpiralMatrix.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class SpiralMatrix
+{
+    public static int[,] GetMatrix(int size)
+    {
+        int[,] arr2d = new int[3,2]{ 
+                                {1, 2}, 
+                                {3, 4}, 
+                                {5, 6} 
+                            };
+        return arr2d;
+    }
+}
